faa9390180d5c2a2bd716f3a1c892f5c37389a7a
Tai-Vaughn White <Isamutai200@gmail.com>
2020-02-16 00:34:58 -0500

    minor change

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'mudamudamuda'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525' # (or try 4652525)
 
-app.config['MAIL_USERNAME'] = 'eb928e6ed14c48'
-app.config['MAIL_PASSWORD'] = '0aaf3bc231fdef' 
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = '' 
 mail = Mail(app)
 from app import views
