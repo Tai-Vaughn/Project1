8a4f565cd540aa0fc207065be536f5c0b41c5008
Tai-Vaughn White <Isamutai200@gmail.com>
2020-04-01 15:40:17 -0500

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:jojo@localhost/users"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ddbdugprosrwsp:424e2260898e422727e1b3b3c61281f057b01bbf9e5f1fd61a1adf4fa3d236d0@ec2-50-17-178-87.compute-1.amazonaws.com:5432/dau4fpsqfstnqg"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = '/app/static/uploads'
 
