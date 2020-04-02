9bf8635d3f9235bdacce7ef0f54dcceeaaa9ef10
Tai-Vaughn White <Isamutai200@gmail.com>
2020-04-02 13:41:31 -0500

    connection change

--- app/db_data.py
+++ app/db_data.py
@@ -2,10 +2,11 @@ import psycopg2
 
 def getData():
     try:
-        connection = psycopg2.connect(user="lab5",
-                                    password="jojo",
-                                    host="localhost",
-                                    database="users")
+        connection = psycopg2.connect(user="ddbdugprosrwsp",
+                                    password="424e2260898e422727e1b3b3c61281f057b01bbf9e5f1fd61a1adf4fa3d236d0",
+                                    host="ec2-50-17-178-87.compute-1.amazonaws.com",
+                                    port="5432"
+                                    database="dau4fpsqfstnqg")
         cursor = connection.cursor()
         postgreSQL_select_Query = "select * from user_profile"
         cursor.execute(postgreSQL_select_Query)
