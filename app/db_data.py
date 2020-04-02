30140f0b9666a614dd182712d8ca7443ca5a6e10
Tai-Vaughn White <Isamutai200@gmail.com>
2020-04-02 13:44:26 -0500

    bug fix

--- app/db_data.py
+++ app/db_data.py
@@ -5,7 +5,7 @@ def getData():
         connection = psycopg2.connect(user="ddbdugprosrwsp",
                                     password="424e2260898e422727e1b3b3c61281f057b01bbf9e5f1fd61a1adf4fa3d236d0",
                                     host="ec2-50-17-178-87.compute-1.amazonaws.com",
-                                    port="5432"
+                                    port="5432",
                                     database="dau4fpsqfstnqg")
         cursor = connection.cursor()
         postgreSQL_select_Query = "select * from user_profile"
